10daa2426d29e78c7ef384e92492d60372ef7375
Fernando <ferjetix95@hotmail.com>
2015-11-28 04:11:38 +0100

    Mise a jour NotifCtrl

--- public/app/NotifCtrl.js
+++ public/app/NotifCtrl.js
@@ -5,6 +5,7 @@ angular
     this.hours = [];
     this.breaks = [];
     this.message = undefined;
+    this.ind = 1;
 
     function onCalendarNewFile(calendar) {
       this.hours = calendar.heures;
@@ -15,9 +16,7 @@ SocketIOService.onCalendarNewFile(onCalendarNewFile.bind(this));
 
     this.calculateTime = function (){
       this.message = undefined;
-      var now = moment(),
-          ind = 0;
-
+      var now = moment();
       //var hours = ['09:20', '10:50', '12:20', '14:50', '16:20', '17:50'];
       //var breaks = ['10', '10', '70', '10', '10', '0'];
       var closest = moment(this.hours[0], 'HH:mm');
@@ -26,17 +25,21 @@ SocketIOService.onCalendarNewFile(onCalendarNewFile.bind(this));
 
         if ( current > now && current > closest && closest < now ){
             closest = moment(this.hours[i], 'HH:mm');
-            ind = i;
+            console.log("asdsd "+closest.format('HH:mm'));
+            this.ind = i;
+            this.message = undefined;
+        } else if ( !(current > now) && current > closest && closest < now) {
+          closest = moment(this.hours[i], 'HH:mm');
+          console.log("bsdsd " +closest.format('HH:mm'));
+          this.ind = i;
+          this.message = "Allez, partez chez vous !";
         }
       }
 
-      if ( ind === 0 ) {
-        this.message = "Allez, partez chez vous !";
-        console.log("Allez, partez chez vous !");
-      }
-      if (moment(this.hours[ind-1], 'HH:mm') < now) {
-        var endPause = moment(this.hours[ind-1], 'HH:mm').add(this.breaks[ind-1], 'minutes');
-        var bet = moment(this.hours[ind-1], 'HH:mm');
+
+      if (moment(this.hours[this.ind-1], 'HH:mm') < now) {
+        var endPause = moment(this.hours[this.ind-1], 'HH:mm').add(this.breaks[this.ind-1], 'minutes');
+        var bet = moment(this.hours[this.ind-1], 'HH:mm');
 
         if ( now.isBetween(bet, endPause) ) {
           if( endPause.diff(now,'minutes') == 0){
@@ -45,14 +48,14 @@ SocketIOService.onCalendarNewFile(onCalendarNewFile.bind(this));
             this.message = "C'est la pause ! Vous avez encore " + endPause.diff(now, 'minutes') + " minutes de pause." + " Cours @ " + endPause.format('HH:mm');
           }
           console.log("Next @ "+closest.format('HH:mm'));
-          console.log("C'est la pause ! " + ind);
-          if ( this.breaks[ind-1] > 10 ) {
+          console.log("C'est la pause ! " + this.ind);
+          if ( this.breaks[this.ind-1] > 10 ) {
             this.message = "Allez manger !" + " Cours @ " + endPause.format('HH:mm');
             console.log("Allez manger !" + " Cours @ " + endPause.format('HH:mm'));
           }
         }
       } else {
-        var endPause = moment(closest).add(this.breaks[ind], 'minutes');
+        var endPause = moment(closest).add(this.breaks[this.ind], 'minutes');
       }
 
       if( closest.diff(now,'minutes') == 0){
@@ -63,6 +66,7 @@ SocketIOService.onCalendarNewFile(onCalendarNewFile.bind(this));
         this.timeLeft = closest.diff(now,'minutes');
         this.unit = "minutes";
         console.log('Prochaine pause dans ', closest.diff(now,'minutes'),'minutes.');
+
       }
       console.log('La prochaine pause @ : ' + closest.format('HH:mm'));
       this.breakTime = closest.format('HH:mm');
